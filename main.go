d09cc9d961e2dc637b91cae7de69fb003f12f2f6
Matthew Kappus <matthew@Matthews-Air.aps.edu.actd>
2019-02-21 09:09:45 -0700

    move db to ~/data

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	defer f.Close()
 
 	logger := log.New(f, "UpdateLog: ", log.Ldate|log.Lshortfile)
-	rosterDB, err := store.New("rosters.db")
+	rosterDB, err := store.New("/rosters.db")
 
 	if err = rosterDB.DownloadRosters(time.Minute*2, u, p); err != nil {
 		logger.Fatal(err)
--- src/store/store.go
+++ src/store/store.go
@@ -2,6 +2,9 @@ package store
 
 import (
 	"database/sql"
+	"os"
+	"path/filepath"
+	"runtime"
 
 	// ok
 	_ "github.com/mattn/go-sqlite3"
@@ -13,10 +16,26 @@ type Roster struct {
 }
 
 // New returns rosters form provided db
-func New(dbPath string) (*Roster, error) {
-	db, err := sql.Open("sqlite3", dbPath)
+func New(dbName string) (*Roster, error) {
+	dir := filepath.Join(UserHomeDir(), "data")
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.MkdirAll(dir, 0666)
+	}
+	db, err := sql.Open("sqlite3", filepath.Join(dir, dbName))
 	if err != nil {
 		return nil, err
 	}
 	return &Roster{db}, nil
+}
+
+// UserHomeDir returns the windows/unix homedir: Store rosters.db in home/data/rosters.db
+func UserHomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
+	}
+	return os.Getenv("HOME")
 }
